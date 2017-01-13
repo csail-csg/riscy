b8837fd7042db42fc9da591a5af9f8d57812c934
acw1251 <acwright@mit.edu>
2017-01-12 22:44:51 -0500

    Bumped riscv-tests to add mcu test virtual machine.
    All the rv[32|64]-mcu-* tests start at address 0 instead of 0x80000000.
    Updated RV32IM_3stage test scripts to use the new rv[32|64]-mcu-* tests.

--- procs/RV32IM_3stage/runtest.sh
+++ procs/RV32IM_3stage/runtest.sh
@@ -1,6 +1,6 @@
 #!/bin/bash
 
-# Copyright (c) 2016 Massachusetts Institute of Technology
+# Copyright (c) 2016, 2017 Massachusetts Institute of Technology
 
 # Permission is hereby granted, free of charge, to any person
 # obtaining a copy of this software and associated documentation
@@ -22,46 +22,55 @@
 # CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN THE
 # SOFTWARE.
 
+if [ -d $RISCY_TOOLS/riscv32-unknown-elf/share/riscv-tests/isa ] ; then
+    TEST_DIR="$RISCY_TOOLS/riscv32-unknown-elf/share/riscv-tests/isa"
+elif [ -d $RISCY_TOOLS/riscv64-unknown-elf/share/riscv-tests/isa ] ; then
+    TEST_DIR="$RISCY_TOOLS/riscv64-unknown-elf/share/riscv-tests/isa"
+else
+    echo "[ERROR] Can't find tests in \"$RISCY_TOOLS/riscv[32|64]-unknown-elf/share/riscv-tests/isa"
+    return 1
+fi
+
 if [ "$#" -eq 0 ] ; then
     echo "Please select a set of tests:"
     echo "0) full test suite"
     echo ""
     echo "    sanity checks:"
-    echo "1) rv32ui-p-add"
+    echo "1) rv32ui-mcu-add"
     echo ""
     echo "    im tests:"
-    echo "2) rv32ui-p-*"
-    echo "3) rv32um-p-*"
+    echo "2) rv32ui-mcu-*"
+    echo "3) rv32um-mcu-*"
     echo ""
     echo "    privilege tests:"
-    echo "4) rv32mi-p-*"
-    echo "5) rv32si-p-*"
+    echo "4) rv32mi-mcu-*"
+    echo "5) rv32si-mcu-*"
     read OPTION
 else
     OPTION=$1
 fi
 
-RUNEXE="./verilator/bin/ubuntu.exe --just-trace"
+RUNEXE="./verilator/bin/ubuntu.exe --just-run"
 
 rm -rf out/
 mkdir -p out
 
 case "$OPTION" in
-    0) files=`find isa/rv32ui-p-* -type f ! -name "*.*"`
-       files="$files "`find isa/rv32um-p-* -type f ! -name "*.*"`
-       files="$files "`find isa/rv32mi-p-* -type f ! -name "*.*"`
-       files="$files "`find isa/rv32si-p-* -type f ! -name "*.*"`
+    0) files=`find $TEST_DIR/rv32ui-mcu-* -type f ! -name "*.*"`
+       files="$files "`find $TEST_DIR/rv32um-mcu-* -type f ! -name "*.*"`
+       files="$files "`find $TEST_DIR/rv32mi-mcu-* -type f ! -name "*.*"`
+       files="$files "`find $TEST_DIR/rv32si-mcu-* -type f ! -name "*.*"`
        ;;
-    1) $RUNEXE isa/rv32ui-p-add
+    1) $RUNEXE $TEST_DIR/rv32ui-mcu-add
        files=
        ;;
-    2) files=`find isa/rv32ui-p-* -type f ! -name "*.*"`
+    2) files=`find $TEST_DIR/rv32ui-mcu-* -type f ! -name "*.*"`
        ;;
-    3) files=`find isa/rv32um-p-* -type f ! -name "*.*"`
+    3) files=`find $TEST_DIR/rv32um-mcu-* -type f ! -name "*.*"`
        ;;
-    4) files=`find isa/rv32mi-p-* -type f ! -name "*.*"`
+    4) files=`find $TEST_DIR/rv32mi-mcu-* -type f ! -name "*.*"`
        ;;
-    5) files=`find isa/rv32si-p-* -type f ! -name "*.*"`
+    5) files=`find $TEST_DIR/rv32si-mcu-* -type f ! -name "*.*"`
        ;;
     *) echo "Invalid Test Code"
        exit
